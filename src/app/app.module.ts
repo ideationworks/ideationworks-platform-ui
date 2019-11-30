c2566f875608379337cca23ff63a14a8bbff3249
Lydia <lattwood@rams-pro.com>
2019-11-30 12:49:28 -0700

    More changes to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,23 @@ import { SharedModule }            from '../shared/shared.module';
 import { AboutComponent }          from './about/about.component';
 
 import { AppComponent }         from './app.component';
-import { HomeModule }           from './home/home.module';
 import { IdeasModule }          from './ideas/ideas.module';
 import { HeaderComponent }      from './layout/header/header.component';
 import { LeftToolbarComponent } from './layout/left-toolbar/left-toolbar.component';
 import { OauthModule }          from './oauth/oauth.module';
 import { SignupModule }         from './signup/signup.module';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
 
     declarations: [
 
         AboutComponent,
+        HomeComponent,
         AppComponent,
         HeaderComponent,
-        LeftToolbarComponent
+        LeftToolbarComponent,
+
 
     ],
 
@@ -29,7 +31,6 @@ import { SignupModule }         from './signup/signup.module';
 
         BrowserModule,
         BrowserAnimationsModule,
-        HomeModule,
         HttpClientModule,
         IdeasModule,
         OauthModule,
@@ -39,8 +40,13 @@ import { SignupModule }         from './signup/signup.module';
 
                 path: 'about',
                 component: AboutComponent
-            }
+            },
 
+            {
+                path: '',
+                pathMatch: 'full',
+                component: HomeComponent
+            }
         ]),
         SharedModule,
         SignupModule
