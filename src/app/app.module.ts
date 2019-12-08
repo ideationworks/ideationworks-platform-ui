63940fb93e469892a3e4389bc7f04e757e71d65b
Lydia <lattwood@rams-pro.com>
2019-12-08 11:10:21 -0700

    Added login component to app module.. properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { HttpClientModule }        from '@angular/common/http';
-import { NgModule }                from '@angular/core';
-import { ReactiveFormsModule }     from '@angular/forms';
-import { BrowserModule }           from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule }            from '@angular/router';
-import { ToastrModule }            from 'ngx-toastr';
-import { SharedModule }            from '../shared/shared.module';
-import { AboutComponent }          from './about/about.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {ToastrModule} from 'ngx-toastr';
+import {SharedModule} from '../shared/shared.module';
+import {AboutComponent} from './about/about.component';
 
-import { AppComponent }         from './app.component';
-import { HomeComponent }        from './home/home.component';
-import { IdeasModule }          from './ideas/ideas.module';
-import { HeaderComponent }      from './layout/header/header.component';
-import { LeftToolbarComponent } from './layout/left-toolbar/left-toolbar.component';
-import { OauthModule }          from './oauth/oauth.module';
-import { ProfileModule }        from './profile/profile.module';
-import { SignupModule }         from './signup/signup.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {IdeasModule} from './ideas/ideas.module';
+import {HeaderComponent} from './layout/header/header.component';
+import {LeftToolbarComponent} from './layout/left-toolbar/left-toolbar.component';
+import {OauthModule} from './oauth/oauth.module';
+import {ProfileModule} from './profile/profile.module';
+import {SignupModule} from './signup/signup.module';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
 
@@ -27,6 +28,7 @@ import { SignupModule }         from './signup/signup.module';
         AppComponent,
         HeaderComponent,
         LeftToolbarComponent,
+        LoginComponent,
 
 
     ],
@@ -57,7 +59,13 @@ import { SignupModule }         from './signup/signup.module';
                 pathMatch: 'full',
                 redirectTo: 'home'
 
-            }
+            },
+
+            {
+                path: 'login',
+                component: LoginComponent
+
+            },
 
         ]),
         SharedModule,
@@ -76,7 +84,7 @@ import { SignupModule }         from './signup/signup.module';
     ],
 
     providers: [],
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
 
 
 })
