71ba1b8d1b52496eeb5271de6b2ebd7a94080e7d
Lydia <lattwood@rams-pro.com>
2019-12-08 21:35:20 -0700

    Got login page on route-link. Extremely measly form created.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent {
+
+    public formGroup: FormGroup = new FormGroup({
+
+        email: new FormControl('', [
+
+            Validators.required,
+            Validators.pattern(/^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/)
+
+        ]),
+
+        password: new FormControl('', [
+
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(255)
+
+        ]),
+
+    });
+
+    public constructor() {
+
+    }
+
+
+    onSubmitClick() {
+        console.log(this.formGroup.value);
+        console.log(this.formGroup.valid);
+    }
+}
+
+
