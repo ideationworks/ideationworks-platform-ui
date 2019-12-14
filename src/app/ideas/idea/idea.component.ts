46c5ce13218dbe1044fd5f387fcaebb2bfa3df4e
Lydia <lattwood@rams-pro.com>
2019-12-14 15:24:15 -0700

    Added social login buttons, front end and back end.

--- src/app/ideas/idea/idea.component.ts
+++ src/app/ideas/idea/idea.component.ts
@@ -1,8 +1,9 @@
-import { Component }      from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IdeasService }   from '../ideas.service';
-import { Idea }           from './idea';
-import { IdeaService }    from './idea.service';
+import { Component }             from '@angular/core';
+import { ActivatedRoute }        from '@angular/router';
+import { NgxuxAutocompleteItem } from '@ngxux/autocomplete';
+import { IdeasService }          from '../ideas.service';
+import { Idea }                  from './idea';
+import { IdeaService }           from './idea.service';
 
 @Component({
     selector: 'app-idea',
@@ -11,7 +12,21 @@ import { IdeaService }    from './idea.service';
 })
 export class IdeaComponent {
 
-    public idea: Idea;
+    public idea: Idea = {
+
+        id: '1234-a23234-arf4a3f34f3-fa34ff33f',
+        name: 'Create trello clone!',
+        description: 'asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. <br><br>asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. <br><br>asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. asdf asdf asdfasf as dfsadfasfa asdfasdfasdf. '
+
+    };
+
+    public tags: Array<NgxuxAutocompleteItem> = [
+
+        { label: 'angular' },
+        { label: 'typescript' },
+        { label: 'javascript' },
+
+    ];
 
     public constructor(private ideasService: IdeasService,
                        private ideaService: IdeaService,
@@ -26,7 +41,7 @@ export class IdeaComponent {
                     ideaService.idea$.next(idea);
 
                     this.idea = idea;
-                    
+
                 });
 
             }
