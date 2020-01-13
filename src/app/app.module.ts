6ad7caa7a907925f1c156c06d625d05b27b57f5a
Lydia <lattwood@rams-pro.com>
2020-01-13 13:23:21 -0700

    Making "Contact Us" dialog and click event for button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { HttpClientModule }        from '@angular/common/http';
-import { NgModule }                from '@angular/core';
-import { ReactiveFormsModule }     from '@angular/forms';
-import { BrowserModule }           from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule }            from '@angular/router';
-import { ToastrModule }            from 'ngx-toastr';
-import { SharedModule }            from '../shared/shared.module';
-import { AboutComponent }          from './about/about.component';
-import { AppComponent }            from './app.component';
-import { HomeComponent }           from './home/home.component';
-import { IdeasModule }             from './ideas/ideas.module';
-import { HeaderComponent }         from './layout/header/header.component';
-import { LeftToolbarComponent }    from './layout/left-toolbar/left-toolbar.component';
-import { LoginComponent }          from './login/login.component';
-import { OauthModule }             from './oauth/oauth.module';
-import { ProfileModule }           from './profile/profile.module';
-import { SignupModule }            from './signup/signup.module';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {ToastrModule} from 'ngx-toastr';
+import {SharedModule} from '../shared/shared.module';
+import {AboutComponent} from './about/about.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {IdeasModule} from './ideas/ideas.module';
+import {HeaderComponent} from './layout/header/header.component';
+import {LeftToolbarComponent} from './layout/left-toolbar/left-toolbar.component';
+import {LoginComponent} from './login/login.component';
+import {OauthModule} from './oauth/oauth.module';
+import {ProfileModule} from './profile/profile.module';
+import {SignupModule} from './signup/signup.module';
+import {ContactcomponentComponent} from './about/contactcomponent/contactcomponent.component';
+import {ContactUsDialogComponent} from './about/contact-us-dialog/contact-us-dialog.component';
 
 @NgModule({
 
@@ -27,6 +29,8 @@ import { SignupModule }            from './signup/signup.module';
         AppComponent,
         HeaderComponent,
         LeftToolbarComponent,
+        ContactcomponentComponent,
+        ContactUsDialogComponent,
 
     ],
 
@@ -81,7 +85,7 @@ import { SignupModule }            from './signup/signup.module';
     ],
 
     providers: [],
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
 
 })
 export class AppModule {
